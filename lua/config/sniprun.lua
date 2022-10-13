eae0be36e4862aac5bff779a9a92c7bcca2b17f2
Peleg Sapir <pelegs@gmail.com>
2022-10-13 20:38:40 +0200

    Added key shortcuts to run sniprun

--- lua/config/sniprun.lua
+++ lua/config/sniprun.lua
@@ -2,9 +2,12 @@
 local M = {}
 
 function M.setup()
-	require 'sniprun'.setup {
+	require("sniprun").setup {
 	}
-  vim.api.nvim_set_keymap("v", "<C-s>", "<cmd>'<,'>SnipRun<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('v', 'fs', '<Plug>SnipRun', {silent = true})
+	vim.api.nvim_set_keymap('n', 'fa', '<Plug>SnipRunOperator', {silent = true})
+	vim.api.nvim_set_keymap('n', 'ff', '<Plug>SnipRun', {silent = true})
+	vim.api.nvim_set_keymap('n', 'fd', '<Plug>SnipClose', {silent = true})
 end
 
 return M
