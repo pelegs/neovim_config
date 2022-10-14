89a7dc7e0e00b821a39a54159a9400bfa4296a61
Peleg Sapir <pelegs@gmail.com>
2022-10-14 15:52:03 +0200

    Changed key bindings for sniprun

--- lua/config/sniprun.lua
+++ lua/config/sniprun.lua
@@ -4,10 +4,10 @@ local M = {}
 function M.setup()
 	require("sniprun").setup {
 	}
-	vim.api.nvim_set_keymap('v', 'fs', '<Plug>SnipRun', {silent = true})
-	vim.api.nvim_set_keymap('n', 'fa', '<Plug>SnipRunOperator', {silent = true})
-	vim.api.nvim_set_keymap('n', 'ff', '<Plug>SnipRun', {silent = true})
-	vim.api.nvim_set_keymap('n', 'fd', '<Plug>SnipClose', {silent = true})
+	vim.api.nvim_set_keymap('v', '<C-s>', '<Plug>SnipRun', {silent = true})
+	vim.api.nvim_set_keymap('n', '<C-s>', '<Plug>SnipRunOperator', {silent = true})
+	vim.api.nvim_set_keymap('n', '<C-c>', '<Plug>SnipRun', {silent = true})
+	vim.api.nvim_set_keymap('n', '<C-d>', '<Plug>SnipClose', {silent = true})
 end
 
 return M
