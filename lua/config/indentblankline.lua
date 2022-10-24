a969c460dbc636a513cbc4bbe6f10a9227215619
Peleg Sapir <pelegs@gmail.com>
2022-10-24 17:06:36 +0200

    Change tab behaviour for .tex files

--- lua/config/indentblankline.lua
+++ lua/config/indentblankline.lua
@@ -17,7 +17,6 @@ function M.setup()
 			space_char_blankline = " ",
 			show_current_context = true,
 			show_current_context_start = true,
-			--[[
 			char_highlight_list = {
 					"IndentBlanklineIndent1",
 					"IndentBlanklineIndent2",
@@ -26,8 +25,49 @@ function M.setup()
 					"IndentBlanklineIndent5",
 					"IndentBlanklineIndent6",
 			},
-			--]]
 	}
 end
+--
+-- function M.setup()
+-- 	vim.cmd [[highlight IndentBlanklineIndent1 guifg=#30465d guibg=#14262b gui=nocombine]]
+-- 	vim.cmd [[highlight IndentBlanklineIndent2 guifg=#30465d guibg=#09262e gui=nocombine]]
+-- 	vim.cmd [[highlight IndentBlanklineIndent3 guifg=#30465d guibg=#161e37 gui=nocombine]]
+
+-- 	require("indent_blankline").setup {
+-- 		char = "▎",
+-- 		char_highlight_list = {
+-- 			"IndentBlanklineIndent1",
+-- 			"IndentBlanklineIndent2",
+-- 			"IndentBlanklineIndent3",
+-- 		},
+-- 		space_char_highlight_list = {
+-- 			"IndentBlanklineIndent1",
+-- 			"IndentBlanklineIndent2",
+-- 			"IndentBlanklineIndent3",
+-- 		},
+-- 		show_trailing_blankline_indent = false,
+-- 	}
+-- end
+
+-- vim.opt.termguicolors = true
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#30465d guibg=#1a1a1a gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#30465d guibg=#282832 gui=nocombine]]
+
+-- function M.setup()
+-- require("indent_blankline").setup {
+-- 		show_current_context = true,
+-- 		show_current_context_start = true,
+-- 		char = "",
+--     char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     show_trailing_blankline_indent = false,
+-- }
+-- end
 
 return M
